488e01a94861926644e93cd03d68bb0f9a83b13e
hamza97benroummane <hamza.benroummane@gmail.com>
2022-12-16 18:09:07 +0100

    2

--- src/main/java/com/benroummane/springbootmicroservice3apigateway/controller/CourseController.java
+++ src/main/java/com/benroummane/springbootmicroservice3apigateway/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("gateway/course")//pre-path
+@RequestMapping("gateway/course")///pre-path
 public class CourseController
 {
     @Autowired
